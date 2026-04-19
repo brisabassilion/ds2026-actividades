7c511ab3862a12a6129232081424f791c212d46e
brisabassilion <bribassilion@gmail.com>
2026-04-19 15:54:44 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+const nombre = "Brisa";
+const edad = 21;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+for (let i = 0; i < 3; i++){
+    contador ++;
+}
+console.log(`El valor final del contador es: ${contador}`);
+
+
