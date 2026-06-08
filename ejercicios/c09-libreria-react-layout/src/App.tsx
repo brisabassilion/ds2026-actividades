b973dd1b010c8c1a22ca8cd55bc60146f38610c0
brisabassilion <bribassilion@gmail.com>
2026-06-08 09:47:38 -0300

    feat: agregar Layout

--- ejercicios/c09-libreria-react-layout/src/App.tsx
+++ ejercicios/c09-libreria-react-layout/src/App.tsx
@@ -1,8 +1,13 @@
+import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import './App.css'
 
 function App() {
-  return <Home />
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
 
 export default App
--- ejercicios/c09-libreria-react-layout/src/pages/Home.tsx
+++ ejercicios/c09-libreria-react-layout/src/pages/Home.tsx
@@ -1,26 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { NavBar } from "../components/Layout/Navbar";
-import { Hero } from "../components/Layout/Hero";
-import { LibroCard } from "../components/Layout/LibroCard";
-import { Footer } from "../components/Layout/Footer";
+import { Hero } from "../components/Hero";
+import { LibroCard } from "../components/LibroCard";
 import libros from "../types/libros";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   return (
     <>
-      <NavBar />
-      <Hero />
-      <Container className="container">
-        <h2>Libros Destacados</h2>
-        <Row className="g-4">
-          {libros.map((libro, index) => (
-            <Col key={index} xs={12} sm={6} md={4}>
-              <LibroCard title={libro.title} author={libro.author} img={libro.img} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-      <Footer />
+        <Hero />
+        <Container className="container">
+          <h2>Libros Destacados</h2>
+          <Row className="g-4">
+            {libros.map((libro, index) => (
+              <Col key={index} xs={12} sm={6} md={4}>
+                <LibroCard title={libro.title} author={libro.author} img={libro.img} />
+              </Col>
+            ))}
+         </Row>
+        </Container>
     </>
   );
 }
