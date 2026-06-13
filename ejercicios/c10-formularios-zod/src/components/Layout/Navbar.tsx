373b7bfab354075832547f6745eaffda3715e1d9
brisabassilion <bribassilion@gmail.com>
2026-06-13 16:22:15 -0300

    Alta de libro con validacion

---
+++ ejercicios/c10-formularios-zod/src/components/Layout/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export const NavBar: React.FC = () => (
+   <Navbar className="navbar">
+    <Container>
+      <Navbar.Brand href="#">Rayuela 📚</Navbar.Brand>
+      <Nav className="navbar-links">
+        <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+        <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
+        <Nav.Link as={Link} to="/libros/nuevo">Agregar Libro</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+); 
---
+++ ejercicios/c10-formularios-zod/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Hero } from "../components/Hero";
+import { LibroCard } from "../components/LibroCard";
+import libros from "../types/libros";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Home() {
+  return (
+    <>
+        <Hero />
+        <Container className="container-fluid">
+          <h2>Libros Destacados</h2>
+          <Row className="g-4">
+            {libros.map((libro) => (
+              <Col key={libro.id} xs={12} sm={6} md={4}>
+                <LibroCard id={libro.id} title={libro.title} author={libro.author} img={libro.img} />
+              </Col>
+            ))}
+         </Row>
+        </Container>
+    </>
+  );
+}
+
+export default Home;
